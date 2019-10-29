a165ce3d068a86afa8f8b46e98eb25557fff3ebc
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-29 14:32:29 +0100

    Changes made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+
+const initialTodos = [
+  {
+    id: 'a',
+    task: 'Learn React',
+    complete: true,
+  },
+  {
+    id: 'b',
+    task: 'Learn Firebase',
+    complete: true,
+  },
+  {
+    id: 'c',
+    task: 'Learn GraphQL',
+    complete: false,
+  },
+];
+
+
+const App = () => {
+  const [task, setTask] = useState('');
+  const handleChangeInput = (event) => {
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {initialTodos.map(todo => (
+          <li key={todo.id}>
+            <lable>{todo.task}</lable>
+          </li>
+        ))}
+      </ul>
+      <input type='text' value={task} onChange={handleChangeInput} />
     </div>
   );
 }
