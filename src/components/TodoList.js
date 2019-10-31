aa473a0c6ca4d262d3d9dabdb440d3270948f2e7
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-31 16:00:22 +0100

    'update'

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {TodoContext} from '../App'
 
-
-const TodoList = ({ dispatch, todos }) => (
+const TodoList = ({ todos }) => (
     <ul>
         {todos.map(todo => (
-            <TodoItem key={todo.id} dispatch={dispatch} todo={todo} />
+            <TodoItem key={todo.id} todo={todo} />
         ))}
     </ul>
 )
 
-const TodoItem = ({ dispatch, todo }) => {
+const TodoItem = ({ todo }) => {
+    const dispatch = useContext(TodoContext);
     const handleChange = () =>
         dispatch({
             type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
